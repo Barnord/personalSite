3a4789483d330b10570b77ded5a843fb854b56a3
Ben Arno <bdarno92@gmail.com>
2023-10-30 12:16:02 -0700

    converted to js, still working on carousel.

---
+++ profjsportfolio/pages/components/linksList.js
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import linkData from '../../public/assets/json/links.json';
+
+export default function linksList() {
+  let adrses = linkData.adrses
+  let linkList = [];
+
+  adrses.forEach(adrs => {
+      linkList.push(
+        <li>
+          <Link href={adrs.link} target='_blank'>
+            {adrs.name}
+          </Link>
+        </li>
+      )
+
+    if (adrs.xtra != null) {
+      let xtraLinkList = [];
+
+      adrs.xtra.forEach(xtra =>
+        xtraLinkList.push(
+          <li>
+            <Link href={xtra.link} target='_blank'>
+              {xtra.name}
+            </Link>
+          </li>
+        ));
+      
+      linkList.push(<ul>{xtraLinkList}</ul>)
+      }
+    })
+
+  return(
+    <>
+      <ul>
+        {linkList}
+      </ul>
+    </>
+  );
+}
